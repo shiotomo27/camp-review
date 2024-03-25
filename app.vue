f8830f7d49fb25398bcf85b08815b145e8779239
tomoki shiozawa <shiotomo27@collesta.com>
2024-03-25 15:43:00 +0900

    文言追加

--- app.vue
+++ app.vue
@@ -4,6 +4,9 @@
     style="height: 100vh; width: 100vw; background-color: rgba(0, 0, 0, 0.3)"
   >
     <p class="text-h2">Camp Gear Review</p>
+    <p class="text-body-2 pa-4 font-weight-bold">
+      キャンプギア情報・レビューサイト
+    </p>
     <p class="text-body-2 pa-4 font-weight-bold">現在準備中...</p>
   </div>
 </template>
