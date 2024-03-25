25f0ae8d44ed953dca0b5173361ffe5661cfe08a
tomoki shiozawa <shiotomo27@collesta.com>
2024-03-25 15:40:29 +0900

    初期状態

--- app.vue
+++ app.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div
+    class="d-flex flex-column justify-center align-center"
+    style="height: 100vh; width: 100vw; background-color: rgba(0, 0, 0, 0.3)"
+  >
+    <p class="text-h2">Camp Gear Review</p>
+    <p class="text-body-2 pa-4 font-weight-bold">現在準備中...</p>
   </div>
 </template>
+
+<style lang="scss">
+body {
+  color: white;
+  background-image: url("camping.jpg");
+  background-size: cover;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,17 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  css: ["vuetify/lib/styles/main.sass"],
+  build: {
+    transpile: ["vuetify"],
+  },
+  vite: {
+    define: {
+      "process.env.DEBUG": false,
+    },
+    // for HMR
+    server: {
+      watch: {
+        usePolling: true,
+      },
+    },
+  },
+});
